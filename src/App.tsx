c718d7b98eea18f5d68b8a80238dbbc97228a12f
Joseph Matricciani <jmatricciani22@gmail.com>
2025-04-13 15:48:12 -0700

    boiler plate

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import './styles.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-purple-400'>
+        Hello World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
